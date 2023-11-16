2ac2c4b7530de071193684afd9f725a95bc61853
Daniel Merlo <daniellmerlo@gmail.com>
2023-11-15 21:46:59 -0300

    198. Binding de valores da diretiva com atributos da instância do componente

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <p>Sem a diretiva</p>
   <p v-texto="{ cor: 'red', tamanhoFonte: '200%', totalCaracteres: 25 }">Minha primeira diretiva customizada</p>
-  <p v-texto>Teste</p>
+  <p v-texto="configuracaoTexto">Teste</p>
 </template>
 
 <script>
 
 export default {
-  name: 'App'
+  name: 'App',
+  data: () => ({
+    configuracaoTexto: { cor: 'blue', tamanhoFonte: '300%', totalCaracteres: 25 }
+  })
 }
 </script>
 
